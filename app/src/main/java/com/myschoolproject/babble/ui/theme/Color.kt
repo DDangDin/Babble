eb654545cd4328f0b149b2c00fe9ed7998a3ef88
DDangDin <kcorelec7123@gmail.com>
2023-06-17 18:10:53 +0900

    Initial commit

---
+++ app/src/main/java/com/myschoolproject/babble/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.myschoolproject.babble.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// My Custom Color
+val MainColorTop = Color(0xFFEE7267)
+val MainColorMiddle = Color(0xFFED606B) // 버튼 색 및 LikeList Icon 색
+val MainColorBottom = Color(0xFFEB476E)
+
+val TextDefault = Color(0xFF2D2D2D)
+val TextBig = Color(0xFF282828)
+val TextRegular = Color(0xFF444142)
+val TextHint = Color(0xFF827B7D)
+
